002f6734f757e17a0b4aa4c1d3951b42f6022f47
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-03-24 22:15:57 -0300

    progress bar da aba receitas, sendo desenvolvida

--- activePage.js
+++ activePage.js
@@ -8,3 +8,11 @@ $('#gordSaturadas').mask("#.##0,00", {reverse: true});
 $('#gordTrans').mask("#.##0,00", {reverse: true});
 $('#fibraAlimentar').mask("#.##0,00", {reverse: true});
 $('#sodio').mask("#.##0,00", {reverse: true});
+
+
+
+const passoUm = document.querySelector(".passoUm");
+const passoDois = document.querySelector(".passoDois");
+const passoTres = document.querySelector(".passTres");
+const passoQuatro = document.querySelector(".passoQuatro");
+const btnProx = document.querySelector(".btnCriandoReceita")
